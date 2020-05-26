4e4c71fead81e0b87d56f4cf304ad154b2b9a8d4
Victor Guedes <victorguedes68@hotmail.com>
2020-05-26 17:26:09 -0300

    Map added

--- src/screens/AllSpots.js
+++ src/screens/AllSpots.js
@@ -1,14 +1,19 @@
 import React from "react";
-import {View,Text,Button} from "react-native";
+import {View,Text,Button,Dimensions} from "react-native";
+import MapView, { Marker,LatLng } from "react-native-maps";
+
+const width = Dimensions.get("window").width;
 
 const Home = ({navigation:{navigate}}) => {
+    
     return(
-        <View>
-            <Text>Thats is home</Text>
-            <Button 
-                title="CLICK"
-                onPress={() => navigate("Filter")}
-            />
+        <View style={{height: "60%",position: "relative"}}>
+            <View>
+                <MapView style={{width,height: "100%"}}>
+                    <Marker coordinate={{latitude: 60,longitude: 60}}></Marker>
+                </MapView>
+                
+            </View>
         </View>
     )
 }
